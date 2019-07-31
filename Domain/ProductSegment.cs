203cd43305d1a7a0cb9a7efdd867927086aad212
Pankaj Dobariya <46947596+pddobariya@users.noreply.github.com>
2019-07-31 23:00:06 +0530

    Chnages

---
+++ Domain/ProductSegment.cs
@@ -0,0 +1,41 @@
+﻿using Nop.Core;
+using System;
+
+namespace GBS.Plugin.ProductManagement.Domain
+{
+    /// <summary>
+    /// Represents a segment of product
+    /// </summary>
+    public partial class ProductSegment : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets a name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets a display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets a store identifier
+        /// </summary>
+        public int StoreId { get; set; }
+
+        /// <summary>
+        /// Get or set created on
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Get or set UpdateOnUTC
+        /// </summary>
+        public DateTime UpdatedOnUtc { get; set; }
+    }
+}
